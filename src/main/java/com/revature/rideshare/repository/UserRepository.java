b77b671623e55f1783012008ffd0475ba37ba2b2
Dylancorbus <Dylancorbus@outlook.com>
2018-09-19 10:32:47 -0400

    added the jwt token and service

--- src/main/java/com/revature/rideshare/repository/UserRepository.java
+++ src/main/java/com/revature/rideshare/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.revature.rideshare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.rideshare.user.beans.User;
-
+@Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
 	public User findByUsername(String username);
 }
--- src/main/java/com/revature/rideshare/user/UserApplication.java
+++ src/main/java/com/revature/rideshare/user/UserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class UserApplication {
 	
 	public static void main(String[] args) throws Exception {
