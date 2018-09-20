1b96c7002bea230915839fe9ca210c5c5197eb00
Ian Johnson <ianprime0509@gmail.com>
2018-09-20 09:55:37 -0400

    Add example of how car object might be transmitted

--- src/main/java/com/revature/rideshare/user/beans/Car.java
+++ src/main/java/com/revature/rideshare/user/beans/Car.java
@@ -1,13 +1,19 @@
 package com.revature.rideshare.user.beans;
 
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Car {
-	
+
 	private int id;
 	private User owner;
 	private String make;
 	private String model;
 	private int year;
-	
+
 	public Car() {
 		super();
 	}
@@ -51,7 +57,28 @@ public class Car {
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
-	
 
+	/**
+	 * Gets the owner from a link specified in JSON.
+	 * 
+	 * @param uri the URI linking to the owner
+	 */
+	@JsonProperty("owner")
+	public void getOwnerJson(String uri) {
+		AntPathMatcher matcher = new AntPathMatcher();
+		// TODO: add some input validation or something.
+		int userId = Integer.parseInt(matcher.extractUriTemplateVariables("/users/{id}", uri).get("id"));
+		// TODO: retrieve the user object here.
+		owner = null;
+	}
+	
+	/**
+	 * Sets the owner link property in JSON.
+	 * 
+	 * @return a link to the owner
+	 */
+	@JsonProperty("owner")
+	public String setOwnerJson() {
+		return UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(owner.getId()).toUriString();
+	}
 }
