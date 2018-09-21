7c455da189c6b7f2b4d69884ac25ab474ee0255d
Ian Johnson <ianprime0509@gmail.com>
2018-09-21 13:23:06 -0400

    Fix the rest of the beans using an ugly hack

--- src/main/java/com/revature/rideshare/user/controllers/CarController.java
+++ src/main/java/com/revature/rideshare/user/controllers/CarController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.rideshare.user.beans.Car;
 import com.revature.rideshare.user.beans.ResponseError;
+import com.revature.rideshare.user.jsonbeans.CarConverter;
+import com.revature.rideshare.user.jsonbeans.JsonCar;
 import com.revature.rideshare.user.services.CarService;
 
 @RestController
@@ -20,33 +22,26 @@ public class CarController {
 	@Autowired
 	CarService carService;
 
+	@Autowired
+	CarConverter carConverter;
+
 	@RequestMapping(value = "/cars/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> findById(@PathVariable("id") int id) {
 		Car car = carService.findById(id);
 		return car == null
 				? new ResponseError("Car with ID " + id + " does not exist.").toResponseEntity(HttpStatus.NOT_FOUND)
-				: ResponseEntity.ok(car);
+				: ResponseEntity.ok(carConverter.toJson(car));
 	}
 
 	@RequestMapping(value = "/cars", method = RequestMethod.POST)
-	public ResponseEntity<Car> add(@RequestBody @Valid Car car) {
+	public ResponseEntity<JsonCar> add(@RequestBody @Valid JsonCar car) {
 		car.setId(0);
-		Car result = carService.save(car);
-		if (result != null) {
-			return new ResponseEntity<Car>(result, HttpStatus.CREATED);
-		} else {
-			return new ResponseEntity<Car>(result, HttpStatus.CONFLICT);
-		}
+		return ResponseEntity.ok(carConverter.toJson(carService.save(carConverter.fromJson(car))));
 	}
 
 	@RequestMapping(value = "/cars/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Car> update(@PathVariable("id") int id, @RequestBody @Valid Car car) {
+	public ResponseEntity<JsonCar> update(@PathVariable("id") int id, @RequestBody @Valid JsonCar car) {
 		car.setId(id);
-		Car result = carService.save(car);
-		if (result != null) {
-			return new ResponseEntity<Car>(result, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<Car>(result, HttpStatus.CONFLICT);
-		}
+		return ResponseEntity.ok(carConverter.toJson(carService.save(carConverter.fromJson(car))));
 	}
 }
