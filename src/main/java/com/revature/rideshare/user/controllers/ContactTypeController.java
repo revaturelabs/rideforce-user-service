47a41729306a72f972682b3364fb7bfe06ce4aa1
mollymerritt <merritt.mry@gmail.com>
2018-09-23 16:19:24 -0400

    Added post method

--- src/main/java/com/revature/rideshare/user/controllers/ContactTypeController.java
+++ src/main/java/com/revature/rideshare/user/controllers/ContactTypeController.java
@@ -1,5 +1,31 @@
 package com.revature.rideshare.user.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.rideshare.user.beans.ContactType;
+import com.revature.rideshare.user.services.ContactTypeService;
+
+@RestController
+@RequestMapping("/contact-type")
 public class ContactTypeController {
+	private ContactTypeService contactTypeService;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<ContactType> add(@RequestBody @Valid ContactType type) {
+		type.setId(0);
+		ContactType result = contactTypeService.save(type);
+		if (result != null) {
+			return new ResponseEntity<ContactType>(result, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<ContactType>(result, HttpStatus.CONFLICT);
+		}
+	}
 
 }
