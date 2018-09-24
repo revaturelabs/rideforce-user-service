ad840d3f8786653064a996e041cb1c2e394dc164
Ian Johnson <ianprime0509@gmail.com>
2018-09-24 10:17:02 -0400

    Remove CrossOrigin annotation from LoginController

--- src/main/java/com/revature/rideforce/user/controllers/LoginController.java
+++ src/main/java/com/revature/rideforce/user/controllers/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +19,6 @@ import com.revature.rideforce.user.services.UserService;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin
 public class LoginController {
 	@Autowired
 	private UserService userService;
