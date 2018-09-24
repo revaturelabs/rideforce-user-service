bed886ea40e5e618cc719713c5d8bc1b65917fa0
Ian Johnson <ianprime0509@gmail.com>
2018-09-24 10:42:24 -0400

    Add GET /login endpoint

--- src/main/java/com/revature/rideforce/user/controllers/LoginController.java
+++ src/main/java/com/revature/rideforce/user/controllers/LoginController.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.rideforce.user.beans.ResponseError;
+import com.revature.rideforce.user.beans.User;
 import com.revature.rideforce.user.beans.UserCredentials;
 import com.revature.rideforce.user.exceptions.InvalidCredentialsException;
 import com.revature.rideforce.user.services.UserService;
@@ -23,7 +27,14 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@GetMapping
+	public ResponseEntity<?> getCurrentUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return auth == null ? new ResponseError("Not logged in.").toResponseEntity(HttpStatus.FORBIDDEN)
+				: ResponseEntity.status(HttpStatus.FOUND).location(((User) auth.getPrincipal()).toUri()).build();
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<?> login(@RequestBody @Valid UserCredentials credentials) {
 		try {
 			return ResponseEntity.ok('"' + userService.authenticate(credentials) + '"');
