46747024b48385f5e837f726058cc1a5aa82b300
Ian Johnson <ianprime0509@gmail.com>
2018-09-25 15:55:38 -0400

    Fix WebSecurityConfig

--- src/main/java/com/revature/rideforce/user/security/WebSecurityConfig.java
+++ src/main/java/com/revature/rideforce/user/security/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// Matchers for routes that can be accessed without authentication.
 		RequestMatcher[] allowable = { new AntPathRequestMatcher("/login", "POST"),
-				new AntPathRequestMatcher("/users", "GET"), new AntPathRequestMatcher("/users", "POST"),
+				new AntPathRequestMatcher("/users/**", "GET"), new AntPathRequestMatcher("/users", "POST"),
 				new AntPathRequestMatcher("/offices", "GET"), new AntPathRequestMatcher("/contact-types", "GET"),
 				new AntPathRequestMatcher("/roles", "GET"), new AntPathRequestMatcher("/**", "OPTIONS") };
 
