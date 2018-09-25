088ad4a7f1c89469cc6e31a9d41a8166469fd337
Ian Johnson <ianprime0509@gmail.com>
2018-09-25 15:48:54 -0400

    Fix user endpoint permissions for matching service

--- src/main/java/com/revature/rideforce/user/security/WebSecurityConfig.java
+++ src/main/java/com/revature/rideforce/user/security/WebSecurityConfig.java
@@ -22,9 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// Matchers for routes that can be accessed without authentication.
 		RequestMatcher[] allowable = { new AntPathRequestMatcher("/login", "POST"),
-				new AntPathRequestMatcher("/users", "POST"), new AntPathRequestMatcher("/offices", "GET"),
-				new AntPathRequestMatcher("/contact-types", "GET"), new AntPathRequestMatcher("/roles", "GET"),
-				new AntPathRequestMatcher("/**", "OPTIONS") };
+				new AntPathRequestMatcher("/users", "GET"), new AntPathRequestMatcher("/users", "POST"),
+				new AntPathRequestMatcher("/offices", "GET"), new AntPathRequestMatcher("/contact-types", "GET"),
+				new AntPathRequestMatcher("/roles", "GET"), new AntPathRequestMatcher("/**", "OPTIONS") };
 
 		http.csrf().disable();
 		http.authorizeRequests().requestMatchers(allowable).permitAll().anyRequest().authenticated();
