2ade70a843334a1495a75a46bfb74aa14081460b
Newton Hoac <nhoac001@ucr.edu>
2018-10-02 13:17:33 -0400

    UserCredentials tests

--- src/test/java/com/revature/beanTests/UserCredentialsTest.java
+++ src/test/java/com/revature/beanTests/UserCredentialsTest.java
@@ -1,5 +1,27 @@
 package com.revature.beanTests;
 
-public class UserCredentialsTest {
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
+import com.revature.rideforce.user.beans.UserCredentials;
+
+
+
+public class UserCredentialsTest {
+	
+	UserCredentials uc;
+	
+	@Test
+	public void userCredentialConstructorTest() {
+		uc = new UserCredentials();
+		Assertions.assertThat(uc.getEmail()).isEqualTo(null);
+		Assertions.assertThat(uc.getPassword()).isEqualTo(null);
+	}
+	
+	@Test
+	public void userCredentialConstructorParamTest() {
+		uc = new UserCredentials("email@gmail.com", "password");
+		Assertions.assertThat(uc.getEmail()).isEqualTo("email@gmail.com");
+		Assertions.assertThat(uc.getPassword()).isEqualTo("password");
+	}
 }
