3a832efff641072ccce9e7b00a632fb22af732bc
Crandon <krillinmaster@gmail.com>
2018-10-03 14:27:30 -0400

    Response error model test

---
+++ src/test/java/com/revature/controllerTests/CarControllerTest.java
@@ -0,0 +1,30 @@
+package com.revature.controllerTests;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.revature.rideforce.user.UserApplication;
+import com.revature.rideforce.user.controllers.CarController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class CarControllerTest {
+	
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@Test
+	public void loggedOutUserCannotAccessSecureEndpoint() throws Exception {
+		this.mockMvc.perform(get("/cars")).andExpect(status().isForbidden());
+	}
+	
+}
