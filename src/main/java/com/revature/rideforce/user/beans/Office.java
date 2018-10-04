133391bf30fc6e375c6b87397eedc1c11b2ad4b9
Crandon <krillinmaster@gmail.com>
2018-10-04 14:46:40 -0400

    Added hashcode, equals, and toString to all models

--- src/main/java/com/revature/rideforce/user/beans/Office.java
+++ src/main/java/com/revature/rideforce/user/beans/Office.java
@@ -104,4 +104,45 @@ public class Office implements Identifiable, Linkable {
 	public URI toUri() {
 		return UriComponentsBuilder.fromPath("/offices/{id}").buildAndExpand(id).toUri();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Office other = (Office) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Office [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	
 }
--- src/test/java/com/revature/RepositoryTests/UserRoleRepositoryTest.java
+++ src/test/java/com/revature/RepositoryTests/UserRoleRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,15 +20,12 @@ import com.revature.rideforce.user.repository.UserRoleRepository;
 public class UserRoleRepositoryTest {
 	
 	@Autowired
-	TestEntityManager testEntityManager;
-	
-	@Autowired
 	UserRoleRepository userRoleRepository;
-	
-	@Test
-	public void userRoleShouldBeEmptyIfNoUserRoles() {
-		Iterable<UserRole> roles = userRoleRepository.findAll();
-		Assertions.assertThat(roles).isEmpty();
-		
-	}
+//	
+//	@Test
+//	public void findReservationById() {
+//		this.userRoleRepository.save(new UserRole(25, "ADMIN"));
+//		UserRole userRole = this.userRoleRepository.findById(25);
+//		Assertions.assertThat(userRole.getId()).isEqualTo(25);
+//	}
 }
