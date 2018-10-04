985bec1371b7f3384145642cb7cfb8639be94746
Crandon <krillinmaster@gmail.com>
2018-10-04 15:14:50 -0400

    Working on Repository layer test

--- src/main/java/com/revature/rideforce/user/beans/User.java
+++ src/main/java/com/revature/rideforce/user/beans/User.java
@@ -323,4 +323,117 @@ public class User implements UserDetails, Identifiable, Linkable {
 	public URI toUri() {
 		return UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(id).toUri();
 	}
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (active ? 1231 : 1237);
+	result = prime * result + ((address == null) ? 0 : address.hashCode());
+	result = prime * result + ((batchEnd == null) ? 0 : batchEnd.hashCode());
+	result = prime * result + ((bio == null) ? 0 : bio.hashCode());
+	result = prime * result + ((cars == null) ? 0 : cars.hashCode());
+	result = prime * result + ((contactInfo == null) ? 0 : contactInfo.hashCode());
+	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+	result = prime * result + id;
+	result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+	result = prime * result + ((office == null) ? 0 : office.hashCode());
+	result = prime * result + ((password == null) ? 0 : password.hashCode());
+	result = prime * result + ((photoUrl == null) ? 0 : photoUrl.hashCode());
+	result = prime * result + ((role == null) ? 0 : role.hashCode());
+	result = prime * result + ((venmo == null) ? 0 : venmo.hashCode());
+	return result;
+}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (active != other.active)
+			return false;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (batchEnd == null) {
+			if (other.batchEnd != null)
+				return false;
+		} else if (!batchEnd.equals(other.batchEnd))
+			return false;
+		if (bio == null) {
+			if (other.bio != null)
+				return false;
+		} else if (!bio.equals(other.bio))
+			return false;
+		if (cars == null) {
+			if (other.cars != null)
+				return false;
+		} else if (!cars.equals(other.cars))
+			return false;
+		if (contactInfo == null) {
+			if (other.contactInfo != null)
+				return false;
+		} else if (!contactInfo.equals(other.contactInfo))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (office == null) {
+			if (other.office != null)
+				return false;
+		} else if (!office.equals(other.office))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (photoUrl == null) {
+			if (other.photoUrl != null)
+				return false;
+		} else if (!photoUrl.equals(other.photoUrl))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (venmo == null) {
+			if (other.venmo != null)
+				return false;
+		} else if (!venmo.equals(other.venmo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password="
+			+ password + ", photoUrl=" + photoUrl + ", bio=" + bio + ", active=" + active + ", role=" + role
+			+ ", office=" + office + ", address=" + address + ", batchEnd=" + batchEnd + ", cars=" + cars + ", venmo="
+			+ venmo + ", contactInfo=" + contactInfo + "]";
+	}
+	
+	
 }
--- src/test/java/com/revature/RepositoryTests/UserRoleRepositoryTest.java
+++ src/test/java/com/revature/RepositoryTests/UserRoleRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.revature.rideforce.user.UserApplication;
@@ -15,17 +16,14 @@ import com.revature.rideforce.user.beans.UserRole;
 import com.revature.rideforce.user.repository.UserRoleRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = UserApplication.class)
 @DataJpaTest
 public class UserRoleRepositoryTest {
 	
 	@Autowired
 	UserRoleRepository userRoleRepository;
-//	
-//	@Test
-//	public void findReservationById() {
-//		this.userRoleRepository.save(new UserRole(25, "ADMIN"));
-//		UserRole userRole = this.userRoleRepository.findById(25);
-//		Assertions.assertThat(userRole.getId()).isEqualTo(25);
-//	}
+	
+	@Test
+	public void findReservationById() {
+		
+	}
 }
