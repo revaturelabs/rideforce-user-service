5debb50f3a750ce0586803fe40faf08024f08b93
Crandon <krillinmaster@gmail.com>
2018-10-05 13:55:19 -0400

    Added ContactType and ContactInfo repository test

--- src/test/java/com/revature/repositoryTests/ContactInfoRepositoryTest.java
+++ src/test/java/com/revature/repositoryTests/ContactInfoRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.revature.repositoryTests;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.rideforce.user.UserApplication;
+import com.revature.rideforce.user.beans.ContactInfo;
+import com.revature.rideforce.user.beans.ContactType;
+import com.revature.rideforce.user.beans.User;
 import com.revature.rideforce.user.repository.ContactInfoRepository;
 import com.revature.rideforce.user.repository.UserRepository;
 
@@ -28,5 +33,50 @@ public class ContactInfoRepositoryTest {
 	public void validate() {
 		Assertions.assertThat(repository).isNotNull();
 		Assertions.assertThat(userRepo).isNotNull();
+	}
+	
+	@Test
+	public void contactInfoRepositoryCanFindById() {
+		User user = userRepo.findById(1);
+		ContactInfo contactInfo = new ContactInfo();
+		contactInfo.setId(60);
+		contactInfo.setInfo("2705371242");
+		contactInfo.setUser(user);
+		contactInfo.setType(new ContactType(100, "phone"));
+		ContactInfo returnInfo = repository.save(contactInfo);
+		Assertions.assertThat(returnInfo).isNotNull();
+		Assertions.assertThat(repository.findById(returnInfo.getId())).isNotNull();
+		Assertions.assertThat(repository.findById(returnInfo.getId())).isEqualTo(returnInfo);
+	}
+	
+	@Test
+	public void contactInfoRepositoryCanFindByUserId() {
+		User user = userRepo.findById(1);
+		ContactInfo contactInfo = new ContactInfo();
+		contactInfo.setId(60);
+		contactInfo.setInfo("270537676");
+		contactInfo.setUser(user);
+		contactInfo.setType(new ContactType(100, "phone"));
+		ContactInfo returnInfo = repository.save(contactInfo);
+		Assertions.assertThat(returnInfo).isNotNull();
+		Assertions.assertThat(repository.findByUserId(returnInfo.getUser().getId())).isNotNull();
+	}
+	
+	@Test
+	public void nonExistantFindByIdReturnsNull() {
+		User user = userRepo.findById(1);
+		ContactInfo contactInfo = new ContactInfo();
+		contactInfo.setId(60);
+		contactInfo.setInfo("2705376767");
+		contactInfo.setUser(user);
+		contactInfo.setType(new ContactType(100, "phone"));
+		ContactInfo returnInfo = repository.save(contactInfo);
+		Assertions.assertThat(returnInfo).isNotNull();
+		Assertions.assertThat(repository.findById(99999)).isNull();
+	}
+	
+	@Test
+	public void nonExistantUserReturnsNullWhenSearchingByUserId() {
+		Assertions.assertThat(repository.findByUserId(99999)).isEmpty();
 	}
 }
