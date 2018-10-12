22f5b7231a13411ad408f0d1851329c84c932baf
Crandon <krillinmaster@gmail.com>
2018-10-12 09:00:15 -0400

    Fixed the issue with contact info and cars

--- src/main/java/com/revature/rideforce/user/beans/Car.java
+++ src/main/java/com/revature/rideforce/user/beans/Car.java
@@ -177,57 +177,5 @@ public class Car implements Identifiable, Linkable {
 	public URI toUri() {
 		return UriComponentsBuilder.fromPath("/cars/{id}").buildAndExpand(id).toUri();
 	}
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((make == null) ? 0 : make.hashCode());
-		result = prime * result + ((model == null) ? 0 : model.hashCode());
-		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-		result = prime * result + year;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Car other = (Car) obj;
-		if (id != other.id)
-			return false;
-		if (make == null) {
-			if (other.make != null)
-				return false;
-		} else if (!make.equals(other.make))
-			return false;
-		if (model == null) {
-			if (other.model != null)
-				return false;
-		} else if (!model.equals(other.model))
-			return false;
-		if (owner == null) {
-			if (other.owner != null)
-				return false;
-		} else if (!owner.equals(other.owner))
-			return false;
-		if (year != other.year)
-			return false;
-		return true;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Car [id=" + id + ", owner=" + owner + ", make=" + make + ", model=" + model + ", year=" + year + "]";
-	}
-	
 	
 }
--- src/main/java/com/revature/rideforce/user/beans/ContactInfo.java
+++ src/main/java/com/revature/rideforce/user/beans/ContactInfo.java
@@ -1,7 +1,6 @@
 package com.revature.rideforce.user.beans;
 
 import java.net.URI;
-import java.util.ArrayList;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,7 +14,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -25,21 +23,10 @@ import com.revature.rideforce.user.json.ContactTypeResolver;
 import com.revature.rideforce.user.json.JsonEnumLike;
 import com.revature.rideforce.user.json.Linkable;
 
-import antlr.collections.List;
-
-
-
-/**
- * bean mapped to the database. Models a user's contact information
- * <p> <strong>Member Variables:</strong> <code><br>int id, <br>User user,<br> {@linkplain com.revature.rideforce.user.beans.ContactType ContactType} type</code>,<br> <code>String info</code>
- *@since Iteration 1: 10/01/2018
- */
 @Entity
 @Table(name = "CONTACT_INFO")
 public class ContactInfo implements Identifiable, Linkable {
-	
 	@Id
-	@Min(1)
 	@Column(name = "CONTACT_INFO_ID")
 	@SequenceGenerator(name = "contactinfoid", sequenceName = "contactinfoid")
 	@GeneratedValue(generator = "contactinfoid", strategy = GenerationType.SEQUENCE)
@@ -61,135 +48,43 @@ public class ContactInfo implements Identifiable, Linkable {
 	@Column(length = 100)
 	@NotEmpty
 	private String info;
-	
-	public ContactInfo() {
-		super();
-		this.user = new User();
-		this.type = new ContactType();
-	}
 
-	public ContactInfo(@Min(1) int id, @NotNull @Valid User user, @NotNull @Valid ContactType type,
-			@NotEmpty String info) {
-		super();
-		this.id = id;
-		this.user = user;
-		this.type = type;
-		this.info = info;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.revature.rideforce.user.beans.Identifiable#getId()
-	 */
 	@Override
 	public int getId() {
 		return id;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.revature.rideforce.user.beans.Identifiable#setId(int)
-	 */
 	@Override
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	/** gets the User member variable of this object
-	 * @return <code>User</code> of this object
-	 * @see com.revature.rideforce.user.beans.User User
-	 */
 	public User getUser() {
 		return user;
 	}
 
-	/** sets the User member variable of this object
-	 * @param user the new User object to be saved in the user member variable
-	 */
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-	/** get the contact type member variable
-	 * @return type of the object, which is a ContactType object
-	 * @see com.revature.rideforce.user.beans.ContactType ContactType
-	 */
 	public ContactType getType() {
 		return type;
 	}
 
-	/**Set the contact type member variable
-	 * @param type the new ContactType object to be saved in ContactInfo's member variable type
-	 * @see com.revature.rideforce.user.beans.ContactType ContactType
-	 */
 	public void setType(ContactType type) {
 		this.type = type;
 	}
 
-	/**Get the ContactType.info member variable
-	 * @return <code>String</code> the information for this contact
-	 */
 	public String getInfo() {
 		return info;
 	}
 
-	/**Set the ContactType.info member variable
-	 * @param value a String that specifies the new info to be set for the contact
-	 */
 	public void setInfo(String value) {
 		this.info = value;
 	}
 	
-	/* (non-Javadoc)
-	 * @see com.revature.rideforce.user.json.Linkable#toUri()
-	 */
 	@Override
 	public URI toUri() {
 		return UriComponentsBuilder.fromPath("/contact-info/{id}").buildAndExpand(id).toUri();
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((info == null) ? 0 : info.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ContactInfo other = (ContactInfo) obj;
-		if (id != other.id)
-			return false;
-		if (info == null) {
-			if (other.info != null)
-				return false;
-		} else if (!info.equals(other.info))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "ContactInfo [id=" + id + ", user=" + user + ", type=" + type + ", info=" + info + "]";
-	}
-	
-	
 }
