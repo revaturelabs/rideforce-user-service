bcfcb25bdfa0dd26860441371910f346c0979207
0x40 <nhoac001@ucr.edu>
2018-10-16 16:31:12 -0400

    Delete FrontEndTokenController.java

--- src/main/java/com/revature/rideforce/user/controllers/FrontEndTokenController.java
+++
@@ -1,8 +0,0 @@
-//package com.revature.rideforce.user.controllers;
-//
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class FrontEndTokenController {
-//	
-//}
