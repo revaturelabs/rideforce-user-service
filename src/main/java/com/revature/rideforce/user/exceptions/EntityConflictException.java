9e5b6438c4613d1f530b09dff6e466fe6d40745a
jbki <jbki@protonmail.com>
2018-10-17 14:14:18 -0400

    made custom exception for logging

--- src/main/java/com/revature/rideforce/user/exceptions/EntityConflictException.java
+++ src/main/java/com/revature/rideforce/user/exceptions/EntityConflictException.java
@@ -4,7 +4,7 @@ package com.revature.rideforce.user.exceptions;
  * The abstract superclass for all exceptions indicating that a conflict has
  * arisen when trying to add or update an entity.
  */
-public abstract class EntityConflictException extends Exception {
+public abstract class EntityConflictException extends CustomException {
 	private static final long serialVersionUID = 1L;
 
 	public EntityConflictException(String message) {
