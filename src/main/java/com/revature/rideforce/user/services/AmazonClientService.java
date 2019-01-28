1af4ff8251ce035a9babe596a150f001c5020a42
baeiten <baeiten@gmail.com>
2019-01-28 09:49:35 -0500

    added get file

--- src/main/java/com/revature/rideforce/user/services/AmazonClientService.java
+++ src/main/java/com/revature/rideforce/user/services/AmazonClientService.java
@@ -1,9 +1,7 @@
 package com.revature.rideforce.user.services;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.text.SimpleDateFormat;
 
@@ -16,19 +14,24 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 @Service
 public class AmazonClientService {
     
     static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private AmazonS3 s3client;
+    /*
     @Value("http://rideshare-photos.s3-website-us-east-1.amazonaws.com")
     private String endpointUrl;
+    */
+    @Value("us-east-1")
+    private String regionName;
     @Value("rideshare-photos")
     private String bucketName;
     @Value("${ACCESS}")
@@ -38,19 +41,18 @@ public class AmazonClientService {
     @PostConstruct
     private void initializeAmazon() {
        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-       this.s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+       this.s3client = AmazonS3ClientBuilder
+    		   .standard()
+    		   .withRegion(Regions.US_EAST_1)
+    		   .withCredentials(new AWSStaticCredentialsProvider(credentials))
+    		   .build();
     }
-    /*
-    public File getFileByUserURL(String url) throws IOException {
+    
+    public S3ObjectInputStream getFileByKey(String key) throws IOException {    	
     	
-    	S3Object getter = s3client.getObject(endpointUrl, url);
-    	
-    	InputStream initialStream = getter.getObjectContent();
+    	return s3client.getObject(bucketName, key).getObjectContent();
+    }
 
-    		    
-    	return null;
-    }
-    */
     /**
      * Rename the file with the timestamp so we can upload the same file multiple times
      * @param multiPart
@@ -79,21 +81,23 @@ public class AmazonClientService {
     }
     
     public String uploadFile(MultipartFile multipartFile) {
-      String fileUrl = "";
+      String fileName = "";
       try {
           File file = convertMultiPartToFile(multipartFile);
-          String fileName = generateFileName(multipartFile);
-          fileUrl = endpointUrl + "/" + fileName;
+          
+          fileName = generateFileName(multipartFile);
+
           uploadFileTos3bucket(fileName, file);
           file.delete();
+          
       } catch (Exception e) {
           logger.error(e.getMessage());
       }
-      return fileUrl;
+      return fileName;
     }
     
-    public String deleteFileFromS3Bucket(String fileUrl) {
-       String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+    public String deleteFileFromS3Bucket(String fileName) {
+    	
        s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
        return "Successfully deleted";
     }
