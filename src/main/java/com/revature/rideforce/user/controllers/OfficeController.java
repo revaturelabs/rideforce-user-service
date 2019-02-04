47cf2b8455e32b1bedbe1a74115df748660fc1d4
Mateusz Wiater <mateuszwiater@gmail.com>
2019-02-04 18:36:09 -0500

    Fixed CORS.

--- src/main/java/com/revature/rideforce/user/controllers/OfficeController.java
+++ src/main/java/com/revature/rideforce/user/controllers/OfficeController.java
@@ -3,15 +3,14 @@ package com.revature.rideforce.user.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.rideforce.user.beans.Office;
 import com.revature.rideforce.user.services.OfficeService;
-@CrossOrigin(origins="*")
-@RestController
+
 @Lazy(true)
+@RestController
 @RequestMapping("/offices")
 @PreAuthorize("hasAnyRole('ROLE_TRAINER','ROLE_ADMIN','ROLE_RIDER', 'ROLE_DRIVER')")
 public class OfficeController extends CrudController<Office> {
