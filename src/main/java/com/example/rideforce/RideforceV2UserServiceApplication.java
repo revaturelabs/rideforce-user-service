8ebcd32e898d634e3ce54ed68b7088b4de10e907
Michael Rollberg <mrollberg@ymail.com>
2019-10-23 15:38:41 -0400

    Created new spring project

---
+++ src/main/java/com/example/rideforce/RideforceV2UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rideforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideforceV2UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideforceV2UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rideforce/RideforceV2UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rideforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideforceV2UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
