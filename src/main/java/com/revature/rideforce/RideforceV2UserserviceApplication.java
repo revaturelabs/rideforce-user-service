b4e57285852cb4bc1c1130505063f4972c1e43da
Dmonroe3795 <32281965+Dmonroe3795@users.noreply.github.com>
2019-10-23 15:12:23 -0400

    Create RideforceV2UserserviceApplication.java

---
+++ src/main/java/com/revature/rideforce/RideforceV2UserserviceApplication.java
@@ -0,0 +1,14 @@
+  
+package com.revature.rideforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideforceV2UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideforceV2UserserviceApplication.class, args);
+	}
+
+}
