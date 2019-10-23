7866536e2dcf59e56d011fb3a8da862030b4ef0b
Dmonroe3795 <32281965+Dmonroe3795@users.noreply.github.com>
2019-10-23 14:44:04 -0400

    Deleted Old Code to Start Fresh
    Deleted old code (the right branch this time) to start again

--- src/main/java/com/revature/rideforce/user/exceptions/DisabledAccountException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.rideforce.user.exceptions;
-
-public class DisabledAccountException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public DisabledAccountException() {
-		super("Account is disabled.");
-	}
-}
--- src/main/java/com/revature/rideforce/user/exceptions/EmptyPasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.rideforce.user.exceptions;
-
-public class EmptyPasswordException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public EmptyPasswordException() {
-		super("Can not set user's password to an empty password");
-	}
-}
