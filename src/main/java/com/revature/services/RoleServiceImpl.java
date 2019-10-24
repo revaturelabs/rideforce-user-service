2f241a550d06062f7d0c610df4e2081ef78d2aa8
krishnakafley <krishnakafleyp@gmail.com>
2019-10-24 10:35:14 -0400

    some test data added to roles via roledata.sql file

--- src/main/java/com/revature/services/RoleServiceImpl.java
+++ src/main/java/com/revature/services/RoleServiceImpl.java
@@ -1,7 +1,5 @@
 package com.revature.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.models.Role;
---
+++ src/main/resources/roledata.sql
@@ -0,0 +1,3 @@
+Insert into Roles(rid, rname) values(1,'Driver'),
+Insert into Roles(id, rname) values(2,'Rider');
+commit;
